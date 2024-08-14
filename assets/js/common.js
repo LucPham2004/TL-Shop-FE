671b44763ecdd707872b1564638a751ff01ebd2a
Luc <phamtienluc0601@gmail.com>
2024-08-14 20:53:48 +0700

    trying to deploy correctly...

--- assets/js/common.js
+++ assets/js/common.js
@@ -10,12 +10,12 @@ document.addEventListener("DOMContentLoaded", function() {
             </a>
             <div class="header-menu d-none d-lg-flex">
                 <a href="../index.html"><div class="header-item">Home</div></a>
-                <a href="../public/shop.html"><div class="header-item" style="width:150px;">Tất cả sản phẩm</div></a>
+                <a href="./public/shop.html"><div class="header-item" style="width:150px;">Tất cả sản phẩm</div></a>
                 <h5 style="margin-bottom: 15px; margin-left:10px; align-self:center; color:rgb(89, 89, 89);">|</h5>
-                <a href="../public/shop.html?category=Adidas"><div class="header-item">Adidas</div></a>
-                <a href="../public/shop.html?category=Nike"><div class="header-item">Nike</div></a>
-                <a href="../public/shop.html?category=Thể Thao"><div class="header-item">Thể Thao</div></a>
-                <a href="../public/shop.html?category=Giày Da"><div class="header-item">Giày Da</div></a>
+                <a href="./public/shop.html?category=Adidas"><div class="header-item">Adidas</div></a>
+                <a href="./public/shop.html?category=Nike"><div class="header-item">Nike</div></a>
+                <a href="./public/shop.html?category=Thể Thao"><div class="header-item">Thể Thao</div></a>
+                <a href="./public/shop.html?category=Giày Da"><div class="header-item">Giày Da</div></a>
             </div>
             <div class="header-rightPart">
                 <div class="search-box">
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     <i class="fas fa-cart-plus" style="font-size: 30px;text-align:center;"></i>
                 </div>
                 <div class="account">
-                    <a href="../public/login.html">
+                    <a href="./public/login.html">
                         <div id="login" style="width:max-content">
                             <i class="fas fa-user" style="margin-right: 10px"></i>
                             Sign in
@@ -52,11 +52,11 @@ document.addEventListener("DOMContentLoaded", function() {
             </div>
             <div class="offcanvas-body">
                 <a href="../index.html"><div class="header-item">Home</div></a>
-                <a href="../public/shop.html"><div class="header-item" style="width:150px;">Tất cả sản phẩm</div></a>
-                <a href="../public/shop.html?category=Adidas"><div class="header-item">Adidas</div></a>
-                <a href="../public/shop.html?category=Nike"><div class="header-item">Nike</div></a>
-                <a href="../public/shop.html?category=Thể Thao"><div class="header-item">Thể Thao</div></a>
-                <a href="../public/shop.html?category=Giày Da"><div class="header-item">Giày Da</div></a>
+                <a href="./public/shop.html"><div class="header-item" style="width:150px;">Tất cả sản phẩm</div></a>
+                <a href="./public/shop.html?category=Adidas"><div class="header-item">Adidas</div></a>
+                <a href="./public/shop.html?category=Nike"><div class="header-item">Nike</div></a>
+                <a href="./public/shop.html?category=Thể Thao"><div class="header-item">Thể Thao</div></a>
+                <a href="./public/shop.html?category=Giày Da"><div class="header-item">Giày Da</div></a>
             </div>
         </div>
     </header>
@@ -65,7 +65,7 @@ document.addEventListener("DOMContentLoaded", function() {
     <div class="offcanvas offcanvas-end" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasCart" aria-labelledby="offcanvasCartLabel">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="offcanvasCartLabel">Giỏ Hàng</h5>
-            <a href="../public/payment.html"><div class="orderBtn">Đặt hàng</div></a>
+            <a href="./public/payment.html"><div class="orderBtn">Đặt hàng</div></a>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", function() {
         <div style="background-color: black;display:flex;justify-content:center;margin-top:5%;">
             <div id="a-bar">
                 <p>Rất nhiều sản phẩm bạn có thể lựa chọn</p>
-                <a href="../public/shop.html"><button class="shopNow-btn">Xem Tất Cả Sản Phẩm</button></a>
+                <a href="./public/shop.html"><button class="shopNow-btn">Xem Tất Cả Sản Phẩm</button></a>
             </div>
         </div>
         <div class="container-fluid p-0">
@@ -99,15 +99,15 @@ document.addEventListener("DOMContentLoaded", function() {
                         <a href="">Find a Store</a>
                     </div>
                     <div class="footer-links">
-                        <a href="../public/shop.html">Shop</a>
-                        <a href="../user/profile.html">Order Status</a>
-                        <a href="../user/profile.html">Shipping Information</a>
+                        <a href="./public/shop.html">Shop</a>
+                        <a href="./user/profile.html">Order Status</a>
+                        <a href="./user/profile.html">Shipping Information</a>
                         <a href="">Returns</a>
                     </div>
                     <div class="footer-links">
                         <a href="">About Us</a>
-                        <a href="../public/shop.html">For You</a>
-                        <a href="../user/profile.html">My Account</a>
+                        <a href="./public/shop.html">For You</a>
+                        <a href="./user/profile.html">My Account</a>
                     </div>
                 </div>
                 <div class="para">
@@ -160,8 +160,8 @@ function changeLoginOrProfileHeaderPart() {
                 <i class="fas fa-user-circle"></i> Profile
             </a>
             <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="../user/profile.html">Profile</a></li>
-                <li><a class="dropdown-item" href="../admin/admin.html">Quản lý Website</a></li>
+                <li><a class="dropdown-item" href="./user/profile.html">Profile</a></li>
+                <li><a class="dropdown-item" href="./admin/admin.html">Quản lý Website</a></li>
                 <li><a class="dropdown-item" href="#" onclick="logout()">Đăng xuất<i class="fas fa-arrow-circle-right" style="margin-left: 15px;"></i></a></li>
             </ul>
         </div>
@@ -174,7 +174,7 @@ function changeLoginOrProfileHeaderPart() {
                     <i class="fas fa-user-circle"></i> Profile
                 </a>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="../user/profile.html" onclick="redirectToProfilePageWithJWT()">Profile</a></li>
+                    <li><a class="dropdown-item" href="./user/profile.html" onclick="redirectToProfilePageWithJWT()">Profile</a></li>
                     <li><a class="dropdown-item" href="#" onclick="logout()">Đăng xuất<i class="fas fa-arrow-circle-right" style="margin-left: 15px;"></i></a></li>
                 </ul>
             </div>
@@ -182,7 +182,7 @@ function changeLoginOrProfileHeaderPart() {
     } else {
         // Hiển thị nút đăng nhập
         accountDiv.innerHTML = `
-            <a href="../public/login.html">
+            <a href="./public/login.html">
                 <div id="login" style="width:max-content">
                     <i class="fas fa-user" style="margin-right: 10px"></i>
                     Sign in
